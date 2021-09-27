93ae56415b4009fb37fb99deeb7cf80d6c95364c
William Moura <felipe.1.william@gmail.com>
2021-09-27 13:53:45 -0300

    first commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+# Your Makefile must at least contain the rules $(NAME), all, clean, fclean and re.
+# -Wall, -Wextra and -Werror
+# If your project allows you to use your libft, you must copy its sources and its
+# 	associated Makefile in a libft folder with its associated Makefile. Your project’s
+# 	Makefile must compile the library by using its Makefile, then compile the project.
---
+++ ft_printf.c
@@ -0,0 +1,34 @@
+//PROJECT RULES
+
+//It must not do the buffer management like the real printf
+//It will manage the following conversions: cspdiuxX%
+//It will be compared with the real printf
+//You must use the command ar to create your librairy, using the command libtool
+//	is forbidden.
+
+//Conversions
+	//1. %c print a single character.
+	//2. %s print a string of characters.
+	//3. %p The void * pointer argument is printed in hexadecimal.
+	//4. %d print a decimal (base 10) number.
+	//5. %i print an integer in base 10.
+	//6. %u print an unsigned decimal (base 10) number.
+	//7. %x print a number in hexadecimal (base 16).
+	//8. %% print a percent sign.
+// for more complete references : man 3 printf / man 3 stdarg
+
+//LEARNING GOALS
+//	1. What is variatic arguments and how to use it?
+//	2. How to use github to work colaboratively? (2 workmates team)
+
+//GIT HUB COLLABORATION RULES
+//1. Branches
+//	
+
+
+#include "printf.h"
+
+int	ft_printf(const char *, ...)
+{
+
+}
